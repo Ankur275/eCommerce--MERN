cdbc2a7a6967cf0ad8221fd2d8270966961d883a
Ankur <ankurchakrajit@gmail.com>
2024-05-21 09:18:55 +0530

    second commit

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,13 @@ const errorMiddleware = require("./middleware/error")
 
 const product=require("./routes/productRoute")
 const user = require("./routes/userRoute")
+const order = require("./routes/orderRoute")
+const payment = require("./routes/paymentRoute")
 
 app.use("/api/v1",product)
 app.use("/api/v1",user)
+app.use("/api/v1",order)
+app.use("/api/v1",payment)
 
 app.use(errorMiddleware)
 
